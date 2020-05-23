ea2ebde800835c3960942fe4db45e3f0e4e1c6f2
Martino Pilia <martino.pilia@gmail.com>
2020-05-23 18:18:51 +0200

    Add support for nvim-lsp backend
    Add support to use nvim-lsp, the native neovim LSP client, as a backend
    for vim-ccls.

---
+++ autoload/ccls/lsp/nvim_lsp.vim
@@ -0,0 +1,22 @@
+" Currently it is not possible to call a VimL funcref from Lua. This
+" library allows to register callback funcrefs and later call them
+" through a public function, that is accessible from Lua.
+
+let s:call_id = 0
+let s:callbacks = {}
+
+" Register a callback and return a call id
+function! ccls#lsp#nvim_lsp#register(funcref) abort
+    let s:call_id += 1
+    let s:callbacks[s:call_id] = a:funcref
+    return s:call_id
+endfunction
+
+" Call a registered callback, identified by {call_id}, with given {data}
+function! ccls#lsp#nvim_lsp#callback(call_id, data) abort
+    if has_key(s:callbacks, a:call_id)
+        call call(s:callbacks[a:call_id], [a:data])
+        call remove(s:callbacks, a:call_id)
+    endif
+endfunction
+
---
+++ lua/vim_ccls/init.lua
@@ -0,0 +1,38 @@
+local function warning(message)
+    vim.api.nvim_call_function("ccls#util#warning", {"nvim-lsp error: " .. message})
+end
+
+local function get_ccls(bufnr)
+    clients = vim.lsp.buf_get_clients(bufnr)
+    for _, v in ipairs(clients) do
+        if v["name"] == "ccls" then
+            return v
+        end
+    end
+    return nil
+end
+
+local function request(bufnr, method, params, call_id)
+    ccls = get_ccls(bufnr)
+
+    if ccls == nil then
+        warning("ccls unavailable for buffer " .. bufnr)
+        return
+    end
+
+    status = ccls.request(method, params, function(_, method, result, client_id)
+        if not result then
+            warning("no result from ccls")
+        else
+            vim.api.nvim_call_function("ccls#lsp#nvim_lsp#callback", {call_id, result})
+        end
+    end)
+
+    if not status then
+        warning("failed ccls request")
+    end
+end
+
+return {
+    request = request,
+}
